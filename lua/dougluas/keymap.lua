f4a073a294d0355cee2a8c9e239f016dc131ff0f
Douglas Jordan <douglasjordan2@gmail.com>
2025-06-03 20:55:46 -0500

    add buffer closing / restoring logic

--- lua/dougluas/keymap.lua
+++ lua/dougluas/keymap.lua
@@ -6,14 +6,66 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map('n', '<leader>ll', '<cmd>ALEFix prettier<cr>', { desc = 'Shopify Prettier current buffer. Needs prettier and Shopify prettier installed first.', silent = false})
-vim.keymap.set('n', '>h', function()
-  require('mini.diff').goto_hunk("next")
-end, { desc = 'MiniDiff Next Hunk', silent = true })
-vim.keymap.set('n', '<h', function()
-  require('mini.diff').goto_hunk("prev")
-end, { desc = 'MiniDiff Next Hunk', silent = true })
-vim.keymap.set('n', '<leader>md', ':lua MiniDiff.toggle_overlay()<CR>', { desc = '[M]ini[D]iff toggle overlay', silent = true })
+local closed_buffers = {}
+
+local function is_terminal_buf(buf)
+  return vim.bo[buf].buftype == "terminal"
+end
+
+local function confirm_close_modified(buf)
+  return vim.fn.confirm(
+    "Buffer " .. buf .. " has unsaved changes, are you sure you want to close it?", "&Yes\n&No", 2
+  ) == 1
+end
+
+local function confirm_close_terminal(buf)
+  return vim.fn.confirm(
+    "Buffer " .. buf .. " has a process running, are you sure you want to close it?", "&Yes\n&No", 2
+  ) == 1
+end
+
+function BufClose()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local modified = vim.bo[bufnr].modified
+  local is_term = is_terminal_buf(bufnr)
+
+  if modified then
+    if not confirm_close_modified(bufnr) then
+      return
+    end
+  end
+
+  if is_term then
+    if not confirm_close_terminal(bufnr) then
+      return
+    end
+  end
+
+  table.insert(closed_buffers, {
+    name = vim.api.nvim_buf_get_name(bufnr),
+    filetype = vim.bo[bufnr].filetype,
+    cursor = vim.api.nvim_win_get_cursor(0)
+  })
+
+  local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+  if #buffers > 1 then
+    vim.cmd("bd")
+  else
+    vim.cmd("q")
+  end
+end
+
+function RestoreBuf()
+  local last = table.remove(closed_buffers)
+  if not last then
+    print("No buffer to restore")
+    return
+  end
+
+  vim.cmd("edit " .. vim.fn.fnameescape(last.name))
+  vim.bo.filetype = last.filetype
+  vim.api.nvim_win_set_cursor(0, last.cursor)
+end
 
 map('n', '<C-l>', '<C-i>') --------------------------------------------------------  remap C-i to C-l before remapping l
 
@@ -32,7 +84,9 @@ map('n', '<C-h>', 'gt')  -------------------------------------------------------
 -- buffers
 map('n', '<C-b>', ':enew<CR>')  ---------------------------------------------------  new buffer
 map('n', '<C-n>', ':bnext<CR>')  --------------------------------------------------  next buffer
-map('n', '<leader>q', ':enew<bar>bd #<CR>')  --------------------------------------  quit buffer
+-- map('n', '<leader>q', ':enew<bar>bd #<CR>')  --------------------------------------  quit buffer
+map('n', '<leader>q', ':lua BufClose()<CR>')  -------------------------------------  quit buffer
+map('n', '<leader>Q', ':lua RestoreBuf()<CR>')  -----------------------------------  restore buffer
 
 -- file exporer
 map('n', '<leader>f', ':Oil<CR>')  ------------------------------------------------  open file explorer
@@ -71,4 +125,17 @@ map('n', '<leader><Tab>', 'ddO')  ----------------------------------------------
 map('n', '<leader>s', ':w<CR>')  --------------------------------------------------  save
 map('n', '<leader>qq', ':qa<CR>')  ------------------------------------------------  quit all
 map('n', '<leader>r', ':so %<CR>')  -----------------------------------------------  source
-map('n', '<C-p>', ":%!npx prettier --stdin-filepath %<CR>")  ----------------------  format file
+-- map('n', '<C-p>', ":%!npx prettier --stdin-filepath %<CR>")  ----------------------  format file
+
+map('n', '<C-p>', '<cmd>ALEFix prettier<cr>', { desc = 'Shopify Prettier current buffer. Needs prettier and Shopify prettier installed first.', silent = false})
+
+vim.keymap.set('n', '>h', function()
+  require('mini.diff').goto_hunk("next")
+end, { desc = 'MiniDiff Next Hunk', silent = true })
+
+vim.keymap.set('n', '<h', function()
+  require('mini.diff').goto_hunk("prev")
+end, { desc = 'MiniDiff Next Hunk', silent = true })
+
+vim.keymap.set('n', '<leader>md', ':lua MiniDiff.toggle_overlay()<CR>', { desc = '[M]ini[D]iff toggle overlay', silent = true })
+
